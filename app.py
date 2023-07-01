da74c872c53beed944ed39dcc1c16e3a9fd188a0
Leena Ali <67731093+leenaali1114@users.noreply.github.com>
2023-07-01 22:54:46 +0530

    Uploaded documents for disease prediction

--- app.py
+++ app.py
@@ -703,6 +703,204 @@ def forgot():
     else:
         return render_template("forgot.html")
     
+
+import pickle
+from werkzeug.utils import secure_filename
+import cv2
+import joblib
+import numpy as np
+import imutils
+from tensorflow.keras.applications.vgg16 import preprocess_input
+from tensorflow.keras.models import load_model
+
+# Configuring Flask
+app.config['UPLOAD_FOLDER'] = 'C:/Users/Leena Ali/Documents/DataScienceProjects/Git Basic projects/NAYANA/Doctor-Booking-App-with-Disease-prediction/static/uploads/'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+def preprocess_imgs(set_name, img_size):
+    """
+    Resize and apply VGG-15 preprocessing
+    """
+    set_new = []
+    for img in set_name:
+        img = cv2.resize(img, dsize=img_size, interpolation=cv2.INTER_CUBIC)
+        set_new.append(preprocess_input(img))
+    return np.array(set_new)
+
+
+def crop_imgs(set_name, add_pixels_value=0):
+    """
+    Finds the extreme points on the image and crops the rectangular out of them
+    """
+    set_new = []
+    for img in set_name:
+        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        gray = cv2.GaussianBlur(gray, (5, 5), 0)
+
+        # threshold the image, then perform a series of erosions +
+        # dilations to remove any small regions of noise
+        thresh = cv2.threshold(gray, 45, 255, cv2.THRESH_BINARY)[1]
+        thresh = cv2.erode(thresh, None, iterations=2)
+        thresh = cv2.dilate(thresh, None, iterations=2)
+
+        # find contours in thresholded image, then grab the largest one
+        cnts = cv2.findContours(
+            thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+        c = max(cnts, key=cv2.contourArea)
+
+        # find the extreme points
+        extLeft = tuple(c[c[:, :, 0].argmin()][0])
+        extRight = tuple(c[c[:, :, 0].argmax()][0])
+        extTop = tuple(c[c[:, :, 1].argmin()][0])
+        extBot = tuple(c[c[:, :, 1].argmax()][0])
+
+        ADD_PIXELS = add_pixels_value
+        new_img = img[extTop[1]-ADD_PIXELS:extBot[1]+ADD_PIXELS,
+                      extLeft[0]-ADD_PIXELS:extRight[0]+ADD_PIXELS].copy()
+        set_new.append(new_img)
+
+    return np.array(set_new)
+
+
+
+covid_model = load_model('models/covid.h5')
+braintumor_model = load_model('models/braintumor.h5')
+diabetes_model = pickle.load(open('models/diabetes.sav', 'rb'))
+heart_model = pickle.load(open('models/heart_disease.pickle.dat', "rb"))
+
+@app.route('/disease')
+def disease():
+    return render_template('disease.html')
+
+
+
+@app.route('/covid')
+def covid():
+        return render_template('covid.html')
     
-    if __name__ == "__main__":
+@app.route('/braintumor')
+def brain_tumor():
+        return render_template('braintumor.html')
+    
+@app.route('/diabetes')
+def diabetes():
+        return render_template('diabetes.html')
+    
+@app.route('/heartdisease')
+def heartdisease():
+        return render_template('heartdisease.html')
+    
+
+@app.route('/resultc', methods=['POST'])
+def resultc():
+        if request.method == 'POST':
+            firstname = request.form['firstname']
+            lastname = request.form['lastname']
+            email = request.form['email']
+            phone = request.form['phone']
+            gender = request.form['gender']
+            age = request.form['age']
+            file = request.files['file']
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                flash('Image successfully uploaded and displayed below')
+                img = cv2.imread('static/uploads/'+filename)
+                img = cv2.resize(img, (224, 224))
+                img = img.reshape(1, 224, 224, 3)
+                img = img/255.0
+                pred = covid_model.predict(img)
+                if pred < 0.5:
+                    pred = 0
+                else:
+                    pred = 1
+                # pb.push_sms(pb.devices[0],str(phone), 'Hello {},\nYour COVID-19 test results are ready.\nRESULT: {}'.format(firstname,['POSITIVE','NEGATIVE'][pred]))
+                return render_template('resultc.html', filename=filename, fn=firstname, ln=lastname, age=age, r=pred, gender=gender)
+
+            else:
+                flash('Allowed image types are - png, jpg, jpeg')
+                return redirect(request.url)
+
+
+@app.route('/resultbt', methods=['POST'])
+def resultbt():
+        if request.method == 'POST':
+            firstname = request.form['firstname']
+            lastname = request.form['lastname']
+            email = request.form['email']
+            phone = request.form['phone']
+            gender = request.form['gender']
+            age = request.form['age']
+            file = request.files['file']
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                flash('Image successfully uploaded and displayed below')
+                img = cv2.imread('static/uploads/'+filename)
+                img = crop_imgs([img])
+                img = img.reshape(img.shape[1:])
+                img = preprocess_imgs([img], (224, 224))
+                pred = braintumor_model.predict(img)
+                if pred < 0.5:
+                    pred = 0
+                else:
+                    pred = 1
+                # pb.push_sms(pb.devices[0],str(phone), 'Hello {},\nYour Brain Tumor test results are ready.\nRESULT: {}'.format(firstname,['NEGATIVE','POSITIVE'][pred]))
+                return render_template('resultbt.html', filename=filename, fn=firstname, ln=lastname, age=age, r=pred, gender=gender)
+
+            else:
+                flash('Allowed image types are - png, jpg, jpeg')
+                return redirect(request.url)
+
+
+@app.route('/resultd', methods=['POST'])
+def resultd():
+        if request.method == 'POST':
+            firstname = request.form['firstname']
+            lastname = request.form['lastname']
+            email = request.form['email']
+            phone = request.form['phone']
+            gender = request.form['gender']
+            pregnancies = request.form['pregnancies']
+            glucose = request.form['glucose']
+            bloodpressure = request.form['bloodpressure']
+            insulin = request.form['insulin']
+            bmi = request.form['bmi']
+            diabetespedigree = request.form['diabetespedigree']
+            age = request.form['age']
+            skinthickness = request.form['skin']
+            pred = diabetes_model.predict(
+                [[pregnancies, glucose, bloodpressure, skinthickness, insulin, bmi, diabetespedigree, age]])
+            # pb.push_sms(pb.devices[0],str(phone), 'Hello {},\nYour Diabetes test results are ready.\nRESULT: {}'.format(firstname,['NEGATIVE','POSITIVE'][pred]))
+            return render_template('resultd.html', fn=firstname, ln=lastname, age=age, r=pred, gender=gender)
+
+
+@app.route('/resulth', methods=['POST'])
+def resulth():
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        email = request.form['email']
+        phone = request.form['phone']
+        gender = request.form['gender']
+        nmv = float(request.form['nmv'])
+        tcp = float(request.form['tcp'])
+        eia = float(request.form['eia'])
+        thal = float(request.form['thal'])
+        op = float(request.form['op'])
+        mhra = float(request.form['mhra'])
+        age = float(request.form['age'])
+        print(np.array([nmv, tcp, eia, thal, op, mhra, age]).reshape(1, -1))
+        pred = heart_model.predict(
+            np.array([nmv, tcp, eia, thal, op, mhra, age]).reshape(1, -1))
+        # pb.push_sms(pb.devices[0],str(phone), 'Hello {},\nYour Diabetes test results are ready.\nRESULT: {}'.format(firstname,['NEGATIVE','POSITIVE'][pred]))
+        return render_template('resulth.html', fn=firstname, ln=lastname, age=age, r=pred, gender=gender)
+
+    
+if __name__ == "__main__":
         app.run(debug=False,host="0.0.0.0")
